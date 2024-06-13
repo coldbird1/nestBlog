7d1bac26b786099ef6b7ebe138dba6d5c05c34ee
wbw460284065 <460284065@qq.com>
2024-06-13 17:58:23 +0800

    毁灭吧，累了

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,13 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  overrideConfig: {
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/eslint-config-typescript',
+      '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    plugins: [], // 如果有额外的自定义插件需要在这里列出
+    parserOptions: {
+      ecmaVersion: 'latest'
+    }
   }
 }
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,6 +2,15 @@
 import TheWelcome from '../components/TheWelcome.vue'
 
 const i = 20
+const v = 10
+const a = 10
+const b = 10
+const c = 10
+
+const onClick = () => {
+  console.log('cssslis ck1s55')
+}
+
 </script>
 
 <template>
