a4e7594a9ab9498ab6cb7ee77b6a8b2277671e8d
wbw460284065 <460284065@qq.com>
2024-06-14 15:03:07 +0800

    引入elementplus

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,9 @@ import './assets/styles/index.scss'
 import 'virtual:uno.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +13,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, {
+  locale: locale
+})
 
 app.mount('#app')
--- client/src/views/login.vue
+++ client/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="color-red">8111</div>
   <div class="w-2xl h-2xl">4444</div>
-
+  <el-button size="small" type="primary">aa</el-button>
 </template>
 
 
