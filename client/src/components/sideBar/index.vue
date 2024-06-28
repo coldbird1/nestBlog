6b597e9cf7dc1a1f51e44a61cd405012b04be9b8
wbw460284065 <460284065@qq.com>
2024-06-28 17:42:03 +0800

    首页修改

---
+++ client/src/components/sideBar/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <div>nestBlog</div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped lang="scss"></style>
--- client/src/views/dashboard.vue
+++ client/src/views/dashboard.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    fff
+    <SideBar></SideBar>
+
   </div>
 </template>
 
 
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-
+import SideBar from '@/components/sideBar/index.vue'
 </script>
 
 <style scoped lang="scss"></style>
